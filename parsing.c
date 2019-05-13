7527d468b201b970036d261dc503ae69ce3f87f9
jouzo <jdesclercs@gmail.com>
2019-05-13 22:45:11 +0200

    handle negative number of ants

--- parsing.c
+++ parsing.c
@@ -2,9 +2,10 @@
 
 int parse_ants(t_data *data, char *s)
 {
+
     if (strcmp(s, "0") == 0)
         return (0);
-    else if (atoi(s) == 0)
+    else if (atoi(s) <= 0)
         return (-1);
     else
     {
