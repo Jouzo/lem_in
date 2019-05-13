dffbc66ecc5e21f222400afa4fe503ca2525a1b0
jouzo <jdesclercs@gmail.com>
2019-05-13 12:44:36 +0200

    check vertices name and edges from/to

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -40,12 +40,12 @@ typedef struct				s_data
 **  Parsing functions
 */
 
-int     parse(t_data *data, char *s);
-int     check_hash(t_data *data, char *s);
-int     get_source_sink(t_data *data, char *s, int st);
+int         parse(t_data *data, char *s);
+int         check_hash(t_data *data, char *s);
+int         get_source_sink(t_data *data, char *s, int st);
 
-int	    split_count(char **split);
-int     split_arg(t_data *data, char *s, char ***split);
+int	        split_count(char **split);
+int         split_arg(t_data *data, char *s, char ***split);
 
 /*
 **  List functions
@@ -55,11 +55,13 @@ t_vertices	*ft_vertice_list(char const *name, int x, int y);
 int 		add_vertices(t_vertices *head, char *name, int x, int y);
 void		print_vertices(t_vertices *head);
 void        free_vertices(t_vertices *head);
+int			check_vertices_name(t_vertices *head, char *name);
 
-t_edges	*ft_edge_list(char const *from, size_t f_len, char const *to, size_t t_len);
+t_edges     *ft_edge_list(char const *from, char const *to);
 int    		add_edges(t_edges *head, char *from, char *to);
 void		print_edges(t_edges *head);
 void        free_edges(t_edges *head);
+int			check_edges_names(t_vertices *head, char *from, char *to);
 
 /*
 **  Free functions
