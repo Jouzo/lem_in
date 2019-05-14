1030a6b716556f7df101031c5d15b16dfe65b84f
jouzo <jdesclercs@gmail.com>
2019-05-14 23:55:30 +0200

    handle dash and spaces error

--- parsing/split.c
+++ parsing/split.c
@@ -10,6 +10,34 @@ int		split_count(char **split)
 	return (i);
 }
 
+int		count_spaces(char *s)
+{
+	int count;
+
+	count = 0;
+	while (*s && s)
+	{
+		if (*s == ' ')
+			count++;
+		s++;
+	}
+	return (count == 2);
+}
+
+int		count_dash(char *s)
+{
+	int count;
+
+	count = 0;
+	while (*s && s)
+	{
+		if (*s == '-')
+			count++;
+		s++;
+	}
+	return (count == 1);
+}
+
 int		split_arg(t_data *data, char *s, char ***split)
 {
 	int i;
@@ -17,14 +45,14 @@ int		split_arg(t_data *data, char *s, char ***split)
 	i = 0;
 	while (ft_isalnum(s[i]) && s[i])
 		i++;
-	if (s[i] == ' ')
+	if (s[i] == ' ' && count_spaces(s))
 	{
 		if (data->set_vertices != 0)
 			return (-1);
 		*split = ft_strsplit(s, ' ');
 		return (1);
 	}
-	else if (s[i] == '-')
+	else if (s[i] == '-' && count_dash(s))
 	{
 		if (!data->vertices)
 			return (-1);
