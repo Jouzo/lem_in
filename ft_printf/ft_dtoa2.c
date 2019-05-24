5561ace2bb3fc8350841d9ce83fa34bcbdb113d1
jouzo <jdesclercs@gmail.com>
2019-05-24 11:51:39 +0200

    remake printf

--- ft_printf/ft_dtoa2.c
+++ ft_printf/ft_dtoa2.c
@@ -3,16 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   ft_dtoa2.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmovahhe <mmovahhe@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jdescler <jdescler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 18:41:50 by mmovahhe          #+#    #+#             */
-/*   Updated: 2019/04/22 20:02:35 by mmovahhe         ###   ########.fr       */
+/*   Updated: 2019/05/24 11:51:23 by jdescler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/ft_printf.h"
 
-void				get_sign(long double f, t_args *args)
+static unsigned long	ft_power(unsigned long n, long pow)
+{
+	return (pow > 0 ? n * ft_power(n, pow - 1) : 1);
+}
+
+void					get_sign(long double f, t_args *args)
 {
 	long double	g;
 	char		data[sizeof(long double)];
@@ -24,27 +29,28 @@ void				get_sign(long double f, t_args *args)
 	args->minus = (data[9] & z) ? 1 : 0;
 }
 
-unsigned long		get_deci(long double n, int i)
+unsigned long			get_deci(long double n, int i)
 {
 	long double		decimal;
 	unsigned long	deci;
 	unsigned long	to_round;
 
-	decimal = (n - (unsigned long)n) * ft_pow(10, i);
+	decimal = (n - (unsigned long)n) * ft_power(10, i);
 	deci = (unsigned long)decimal;
-	decimal = (n - (unsigned long)n) * ft_pow(10, i + 1);
+	decimal = (n - (unsigned long)n) * ft_power(10, i + 1);
 	to_round = (unsigned long)decimal;
 	if (to_round % 10 > 4)
 		deci++;
 	return (deci);
 }
 
-unsigned long		get_digit(long double n, int i)
+unsigned long			get_digit(long double n, int i)
 {
 	unsigned long	int_part;
 
 	int_part = n < 0.0 ? (unsigned long)-n : (unsigned long)n;
 	if (i + 1 == 20)
 		return ('1');
-	return (int_part % ft_pow(10, i + 1) / ft_pow(10, i) + '0');
+	return (int_part % ft_power(10, i + 1)
+				/ ft_power(10, i) + '0');
 }
