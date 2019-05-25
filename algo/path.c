6feddd3568f6d430e5156f108e13a97919cd5a5c
Massoud_le_Grand <monsieurmassoud@gmail.com>
2019-05-25 14:41:47 +0200

    implementation of await at the begenning

--- algo/path.c
+++ algo/path.c
@@ -1,11 +1,10 @@
 #include "algo.h"
 
-t_path	*get_path(int *path, t_queue queue, char *edges)
+t_path	*get_path(int *path, t_queue queue, char *edges, int await)
 {
 	int		u;
 	t_path	*aug_path;
 
-	(void)edges;
 	u = queue.capacity - 1;
 	aug_path = init_path(u);
 	if (path[u] == 0 && edges[u] == '0')
@@ -17,6 +16,11 @@ t_path	*get_path(int *path, t_queue queue, char *edges)
 	{
 		u = path[u];
 		push_vertex(&aug_path, u);
+	}
+	while (await)
+	{
+		push_vertex(&aug_path, 0);
+		await--;
 	}
 	free(path);
 	return (aug_path);
@@ -36,7 +40,7 @@ int		get_path_size(int *path, int sink, int vertex)
 	return (size);
 }
 
-t_path	*find_path(t_args *args, t_flow *flow, int stage)
+t_path	*find_path(t_args *args, t_flow *flow, int stage, int await)
 {
 	int vertex;
 	int *path;
@@ -54,7 +58,7 @@ t_path	*find_path(t_args *args, t_flow *flow, int stage)
 		{
 			if (args->edges[vertex * args->queue.capacity + i] == '1'
 			&& check_available(args->state, i) && (i == args->queue.capacity - 1
-					|| check_flow(path, i, flow, stage, vertex)))
+					|| check_flow(path, i, flow, stage, vertex, await)))
 			{
 				path[i] = vertex;
 				enqueue(&args->queue, i);
@@ -63,5 +67,10 @@ t_path	*find_path(t_args *args, t_flow *flow, int stage)
 			i++;
 		}
 	}
-	return (get_path(path, args->queue, args->edges));
+	if (path[args->queue.capacity - 1] == 0 && args->edges[args->queue.capacity - 1] == '0')
+	{
+		await++;
+		return (BFS(args, flow, stage, await));
+	}
+	return (get_path(path, args->queue, args->edges, await));
 }
