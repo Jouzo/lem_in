3d52ee84ae575b14fbeef348c5acff65e34dc099
Massoud MOVAHHED <mmovahhe@e2r12p18.42.fr>
2019-05-26 20:54:35 +0200

    deleting the path taken in reverse

--- algo/path.c
+++ algo/path.c
@@ -1,6 +1,6 @@
 #include "algo.h"
 
-t_path	*get_path(int *path, t_queue queue, char *edges, int await, char **map)
+t_path	*get_path(int *path, t_queue queue, char *edges, char **map)
 {
 	int		u;
 	t_path	*aug_path;
@@ -16,13 +16,13 @@ t_path	*get_path(int *path, t_queue queue, char *edges, int await, char **map)
 	{
 		(*map)[u * queue.capacity + path[u]] = '2';
 		(*map)[path[u] * queue.capacity + u] = '2';
+		if (edges[u * queue.capacity + path[u]] == '0')
+		{
+			(*map)[path[u] * queue.capacity + u] = '0';
+			(*map)[u * queue.capacity + path[u]] = '0';
+		}
 		u = path[u];
 		push_vertex(&aug_path, u);
-	}
-	while (await)
-	{
-		push_vertex(&aug_path, 0);
-		await--;
 	}
 	free(path);
 	return (aug_path);
@@ -42,13 +42,12 @@ int		get_path_size(int *path, int sink, int vertex)
 	return (size);
 }
 
-t_path	*find_path(t_args *args, t_flow *flow, int stage, int await, char **map)
+t_path	*find_path(t_args *args, int stage, char **map)
 {
 	int vertex;
 	int *path;
 	int i;
 	(void)stage;
-	(void)flow;
 
 	if (!(path = malloc(args->queue.capacity * sizeof(int))))
 		return (NULL);
@@ -57,7 +56,6 @@ t_path	*find_path(t_args *args, t_flow *flow, int stage, int await, char **map)
 	{	
 		vertex = dequeue(&args->queue);	
 		change_state(&args->state, vertex, VISITED);
-		// check_reverse(args, vertex, map);
 		i = 1;
 		while (i < args->queue.capacity)
 		{
@@ -71,22 +69,21 @@ t_path	*find_path(t_args *args, t_flow *flow, int stage, int await, char **map)
 					enqueue(&args->queue, i);
 					change_state(&args->state, i, WAITING);
 					if (i == 7 || vertex == 7)
-						return (get_path(path, args->queue, args->edges, await, map));
+						return (get_path(path, args->queue, args->edges, map));
 				}
 				else if (check_map(map, i, vertex, args, stage))
 				{
 					path[i] = vertex;
-					// print_map(*map);
-					// (*map)[vertex * args->queue.capacity + i] = '0';
+					args->edges[vertex * args->queue.capacity + i] = '0';
+					args->edges[i * args->queue.capacity + vertex] = '0';
 					enqueue(&args->queue, i);
 					printf("have to go in reverse i: %d vertex: %d\n", i, vertex);
-					// go_reverse(vertex, i);
 				}
 				// printf("i: %d vertex: %d\n", i, vertex);		
 				// print_queue(args->queue);
 			}
 			i++;
-		}
+		} 
 	}
-	return (get_path(path, args->queue, args->edges, await, map));
+	return (get_path(path, args->queue, args->edges, map));
 }
