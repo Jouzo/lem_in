027ca0814ebbe33a02d3733830684fc45a60f5a9
jouzo <jdesclercs@gmail.com>
2019-06-24 16:10:16 +0200

    get ultimate paths ordered by size

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -7,6 +7,8 @@ char	*stringify(t_data *data);
 char	**split_vertices(t_vertices *head);
 int		output(char *map, t_data *data);
 int		ft_printf(const char *format, ...);
-
+int		get_one_path(int start, int size, t_flow **flow, char *map);
+int		first_path(char *map, int size);
+void	print_output(char *s, char **vertices, t_flow *flow, int ants);
 
 #endif
--- output.c
+++ output.c
@@ -1,77 +1,23 @@
 #include "includes/lem_in.h"
 
-// static char	*formatted_ouput(char *vertex, int ant)
-// {
-// 	char *s;
-// 	int size;
-
-// 	size = ft_sizeofint(ant);
-// 	if (!(s = (char*)malloc(sizeof(char) * (size + ft_strlen(vertex)) + 3)))
-// 		return (NULL);
-// 	s[size + ft_strlen(vertex) + 3] = 0;
-// 	s[0] = 'L';
-// 	ft_strcat(s, ft_itoa(ant));
-// 	s[size + 1] = '-';
-// 	ft_strcat(s, vertex);
-// 	return (s);
-// }
-
-
-// void	print_final(char **vertices, int ant, int i)
-// {
-// 		printf("%s\n", formatted_ouput(vertices[i], ant));
-// }
-
-int		find_next_x(char *map, int start, int size)
+char		**split_vertices(t_vertices *head)
 {
-	int i;
-	int j;
-	int middle;
-	middle = start / size * size + size / 2;
+	char		**split;
+	t_vertices	*tmp;
+	int			i;
 
 	i = 0;
-	j = 0;
-	printf("in next x : value of middle : %d\n", middle);
-	while ((middle + i) / size < size && (middle - i) / size > 0)
+	tmp = head;
+	if (!(split = (char**)malloc(sizeof(char*) * vertices_len(head) + 1)))
+		return (NULL);
+	while (tmp)
 	{
-		if (map[(middle + i)] == '2')
-			return ((middle + i));
-		if (map[(middle - i)] == '2')
-			return ((middle - i));
+		if (!(split[i] = ft_strdup(tmp->name)))
+			return (NULL);
+		tmp = tmp->next;
 		i++;
 	}
-	if (size % 2 == 0)
-	{
-		if (map[(middle + (i + 1))] == '2')
-			return ((middle + (i + 1)));
-	}
-	return (-1);
-}
-
-int		find_next_y(char *map, int start, int size)
-{
-	int i;
-	int j;
-	int middle;
-	middle = size * size / 2 + start % size;
-
-	i = 0;
-	j = 0;
-	printf("value of middle : %d\n", middle);
-	while ((middle + i * size) / size < size && (middle - i * size) / size > 0)
-	{
-		if (map[(middle + i * size)] == '2')
-			return ((middle + i * size));
-		if (map[(middle - i * size)] == '2')
-			return ((middle - i * size));
-		i++;
-	}
-	if (size % 2 == 0)
-	{
-		if (map[(middle + (i + 1) * size)] == '2')
-			return ((middle + (i + 1) * size));
-	}
-	return (-1);
+	return (split);
 }
 
 int		number_of_path(char *map, int size)
@@ -90,80 +36,93 @@ int		number_of_path(char *map, int size)
 	return (count);
 }
 
-int		first_path(char *map, int size)
+void		parse_map(char *map, int size, t_flow **flow)
 {
-	int i;
+	int		i;
+	int 	count;
+	int		paths;
+	int 	first;
 
-	i = 0;
+	paths = number_of_path(map, size);
+	first = first_path(map, size);
+	count = 0;
+	i = first;
+	printf("nb of path %d \n", paths);
 	while (i < size)
 	{
-		if (map[i] == '2')
-			return (i);
+		if ((map)[i] == '2')
+		{
+			get_one_path(i, size, flow, map);
+		}
 		i++;
 	}
-	return (0);
+	print_flow(*flow);
 }
 
-void    parse_map(char *map, char **vertices, int size)
-{	
+// int			recieve_one_path(t_path *path, int *count)
+// {
+// 	int i;
+
+// 	i = 0;
+
+// }
+
+// int			get_output(t_flow *flow, int ants, int nb_path)
+// {
+// 	int i;
+// 	int j;
+// 	char *buf;
+// 	t_flow *tmp;
+// 	char *buf;
+
+// 	j = 0;
+// 	i = 0;
+// 	while (i < ants)
+// 	{
+// 		j = 0;
+// 		while (j < count) {
+// 			recieve_one_path(tmp->path, &count);
+// 			tmp = tmp->next;
+// 			if (j % nb_path == 0)
+// 				tmp = flow;
+// 		}
+// 		count += nb_path;
+// 		i += nb_path;
+// 	}
+// }
+
+int			count_ants_per_path(t_flow *flow)
+{
 	int i;
-	int x_y;
-	t_path *path;
-	t_flow *flow;
-	int j;
-	int br;
-	int count;
-	int k;
-	k = 0;
-	count = 0;
-	br = 0;
-	j = first_path(map, size);
-	(void)vertices;
-	printf("number of path : %d\n", number_of_path(map, size));
-	while (j < size && count < number_of_path(map, size))
+	t_flow *tmp;
+
+	tmp = flow;
+	i = 0;
+	while (tmp->next) 
 	{
-	path = 0;
-	i = k = j;
-	x_y = 0;
-    while (i < size * size)
-    {
-		if (map[i] == '2') {
-		k = !x_y ? find_next_y(map, i, size) : find_next_x(map, i, size);
-			printf("value of k : %d\n", k);
-			printf("i when 2 : %d\n", i);
-			j = i / size == 0 ? i : j;
-			if (!path) {
-				path = init_path(i);
-			}
-			else
-				push_vertex(&path, i);
-			x_y = x_y ? 0 : 1;
-			if ((i % size) + 1 == size || (i / size) + 1 == size)
-				{	
-					if (!count)
-						flow = new_flow(path);
-					else
-						add_flow(flow, path);
-					count++;
-					break ;
-				}
-		}
-		i += !x_y ? 1 : size;
-    }
-	j++;
+		tmp->size = tmp->next->size - tmp->size;
+		tmp = tmp->next;
 	}
 	print_flow(flow);
+	return (1);
 }
 
-int		output(char *map, t_data *data)
-{
-	char **split;
-	int size;
 
+int			output(char *map, t_data *data)
+{
+	char	**split;
+	int		size;
+	t_flow *flow;
+
+	flow = NULL;
 	size = vertices_len(data->vertices);
 	if (!(split = split_vertices(data->vertices)))
 		return (-1);
-	parse_map(map, split, size);
+	parse_map(map, size, &flow);
+	// get_output(flow, data->ants, 2);
+	count_ants_per_path(flow);
+	print_map(map);
+	// print_output(map, split, flow, data->ants);
 	// return (free_split(split, 1));
 	return (1);
 }
