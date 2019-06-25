8c08838e728753ea7cc958321c2c2e4ab08ac17c
Jouzo <jdesclercs@gmail.com>
2019-06-25 12:49:35 +0200

    ants file, get number of ants per path

---
+++ ants.c
@@ -0,0 +1,77 @@
+#include "includes/lem_in.h"
+
+static void		count_ants_per_path(t_flow *flow)
+{
+	int i;
+	t_flow *tmp;
+
+	tmp = flow;
+	i = 0;
+	while (tmp->next) 
+	{
+		tmp->next->size = -(tmp->next->size - flow->size);
+		tmp = tmp->next;
+	}
+	flow->size = 0;
+}
+
+int			*get_ants_tab(t_flow *flow, int nb_path)
+{
+	int		i;
+	t_flow	*tmp;
+	int		*tab;
+
+	if (!(tab = (int*)malloc(sizeof(int) * nb_path)))
+		return (NULL);
+	tmp = flow;
+	i = 0;
+	while (tmp) 
+	{
+		tab[i] = tmp->size;
+		tmp = tmp->next;
+		i++;
+	}
+	return (tab);
+}
+
+int			assign_ants_per_path(t_flow *flow, int *ants)
+{
+	int		i;
+	t_flow	*tmp;
+
+	tmp = flow;
+	i = 0;
+	while (tmp)
+	{
+		tmp->size = ants[i++];
+		tmp = tmp->next;
+	}
+	print_flow(flow);
+	free(ants);
+	return (1);
+}
+
+int			get_ants_per_path(t_flow *flow, int nb_ants, int nb_path)
+{
+	int		i;
+	t_flow	*tmp;
+	int		*ants;
+	int		count;
+
+	count = 0;
+	count_ants_per_path(flow);
+	if (!(ants = get_ants_tab(flow, nb_path)))
+		return (-1);
+	tmp = flow;
+	while (count < nb_ants) 
+	{
+	i = 0;
+		while (i < nb_path && count < nb_ants)
+		{
+			if (ants[i] >= 0)
+				count++;
+			(ants[i++])++;
+		}
+	}
+	return (assign_ants_per_path(flow, ants));
+}
