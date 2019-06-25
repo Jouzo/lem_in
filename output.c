74e3860c5d6b24cafb3a3b5e09671200fab1b68f
Jouzo <jdesclercs@gmail.com>
2019-06-25 12:50:27 +0200

    cleanup

--- output.c
+++ output.c
@@ -20,40 +20,19 @@ char		**split_vertices(t_vertices *head)
 	return (split);
 }
 
-int		number_of_path(char *map, int size)
-{
-	int i;
-	int count;
-
-	i = 0;
-	count = 0;
-	while (i < size)
-	{
-		if (map[i] == '2')
-			count++;
-		i++;
-	}
-	return (count);
-}
-
 void		parse_map(char *map, int size, t_flow **flow)
 {
 	int		i;
 	int 	count;
-	int		paths;
 	int 	first;
 
-	paths = number_of_path(map, size);
 	first = first_path(map, size);
 	count = 0;
 	i = first;
-	printf("nb of path %d \n", paths);
 	while (i < size)
 	{
 		if ((map)[i] == '2')
-		{
 			get_one_path(i, size, flow, map);
-		}
 		i++;
 	}
 	print_flow(*flow);
@@ -91,36 +70,22 @@ void		parse_map(char *map, int size, t_flow **flow)
 // 	}
 // }
 
-int			count_ants_per_path(t_flow *flow)
-{
-	int i;
-	t_flow *tmp;
-
-	tmp = flow;
-	i = 0;
-	while (tmp->next) 
-	{
-		tmp->size = tmp->next->size - tmp->size;
-		tmp = tmp->next;
-	}
-	print_flow(flow);
-	return (1);
-}
-
-
 int			output(char *map, t_data *data)
 {
 	char	**split;
 	int		size;
 	t_flow *flow;
+	int nb_path;
 
 	flow = NULL;
 	size = vertices_len(data->vertices);
+	nb_path = number_of_path(map, size);
 	if (!(split = split_vertices(data->vertices)))
 		return (-1);
 	parse_map(map, size, &flow);
 	// get_output(flow, data->ants, 2);
-	count_ants_per_path(flow);
+	if (get_ants_per_path(flow, data->ants, nb_path) == -1)
+		return (-1);
 	print_map(map);
 	// print_output(map, split, flow, data->ants);
 	// return (free_split(split, 1));
