2af21d50f459bac1c48ecc3f3290213cc4f86633
Jouzo <jdesclercs@gmail.com>
2019-06-25 12:50:09 +0200

    header file

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -7,8 +7,21 @@ char	*stringify(t_data *data);
 char	**split_vertices(t_vertices *head);
 int		output(char *map, t_data *data);
 int		ft_printf(const char *format, ...);
-int		get_one_path(int start, int size, t_flow **flow, char *map);
-int		first_path(char *map, int size);
 void	print_output(char *s, char **vertices, t_flow *flow, int ants);
+
+
+/*
+** path functions
+*/
+
+int		first_path(char *map, int size);
+int		number_of_path(char *map, int size);
+int		get_one_path(int start, int size, t_flow **flow, char *map);
+
+/*
+** ants functions
+*/
+
+int			get_ants_per_path(t_flow *flow, int nb_ants, int nb_path);
 
 #endif
