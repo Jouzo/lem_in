5adb5ddc44b87fe1dd217a81bdb7adcc8e47601e
jouzo <jdesclercs@gmail.com>
2019-06-29 20:12:16 +0200

    quiet flag + division of main

--- src/main.c
+++ src/main.c
@@ -1,18 +1,11 @@
 #include "lem_in.h"
 
-void	print_data(t_data *data)
+static int	check_parsing(t_data data)
 {
-	// // printf("current state :\n");
-	// // printf("source : %s\nsink : %s\nants : %d\n", data->source, data->sink, data->ants);
-	swap_source(data);
-	// if (data->vertices)
-		// print_vertices(data->vertices);
-	// // printf("Edges :\n");
-	// if (data->edges)
-		// print_edges(data->edges);
+	return (data.source && data.sink && data.ants && data.edges);
 }
 
-int   init_data(t_data *data)
+static int   init_data(t_data *data)
 {    
 	char	*line;
 	int 	ret;
@@ -20,7 +13,7 @@ int   init_data(t_data *data)
 	ret = 1;
 	while (get_next_line(0, &line) > 0)
 	{
-		ft_putendl(line);
+		data->flag != QUIET ? ft_putendl(line) : 0;
 		if ((ret = parse(data, line)) <= 0)
 		{
 			ft_memdel((void**)&line);
@@ -28,62 +21,65 @@ int   init_data(t_data *data)
 		}
 		ft_memdel((void**)&line);
 	}
-	// // printf("done parsing\n");
 	return (ret);
+}
+
+static void		lem_in(t_data *data)
+{
+	char *map;
+
+	swap_source(data);
+	map = stringify(data);
+	algo(&map, ft_sqrt(ft_strlen(map)), data->ants);
+	output(map, data);
+	ft_memdel((void**)&map);
+}
+
+static int		get_flags(t_data *data, char *flag)
+{
+	int i;
+
+	i = 1;
+	if (flag[0] != '-')
+		return (-1);
+	while (flag[i])
+	{
+		if (flag[i] == 'q')
+			data->flag = QUIET;
+		else
+			return (-1);
+		i++;
+	}
+	return (1);
 }
 
 int     main(int ac, char **av)
 {
-	int		i;
-	int		ret;
 	t_data data;
-	char *map;
+	
 	ft_bzero(&data, sizeof(t_data));
-	(void)av;
-	(void)ac;
-	i = 1;
-	if ((ret = init_data(&data)) <= 0) 
+	if (ac > 1)
 	{
-		if (ret < 0)
+		if (get_flags(&data, av[1]) < 0 || ac > 2)
 		{
-			if (data.source && data.sink && data.ants && data.edges)
-			{
-
-				// DO LEM-IN ALGO
-				print_data(&data);
-				// // printf("now calculate ret < 0\n");
-				map = stringify(&data);
-				algo(&map, ft_sqrt(ft_strlen(map)), data.ants);
-				output(map, &data);
-				free_data(&data);
-			}
-			else
-			{
-				print_data(&data);
-				free_data(&data);
-				write(1, "Error\n", 5);
-				return (-1);
-			}
-		}
-		else if (ret == 0)
-		{
-			free_data(&data);
-			write(1, "No ants\n", 8);
+			write(1, "usage: ./lem-in [-v] < a lem_in map", 35);
+			return (-1);
 		}
 	}
-	else {
-	// // printf("now calculate ret > 0\n");
-	print_data(&data);
-	map = stringify(&data);
-	algo(&map, ft_sqrt(ft_strlen(map)), data.ants);
-	// printf("\n");
-	// printf("\n");
-	// printf("\n");
-	// print_flow(flow);
-	output(map, &data);
-	// free_flow(flow);
+	if (init_data(&data) <= 0) 
+	{
+		if (check_parsing(data))
+			lem_in(&data);
+		else
+			write(1, "Error\n", 5);
+	}
+	else 
+	{
+		if (check_parsing(data))
+			lem_in(&data);
+		else
+			write(1, "Error\n", 5);
+	}
 	free_data(&data);
-	ft_memdel((void**)&map);
-	}
 	return (0);
 }
