f086fdb403c80f7cef91c7623a15d3a6087c67a3
jouzo <jdesclercs@gmail.com>
2019-07-05 14:43:16 +0200

    valgrind script to test all maps

--- SCRIPT_TEST.sh
+++ SCRIPT_TEST.sh
@@ -3,5 +3,5 @@
 FILES=map_samples/bad_tests/*.map
 for f in $FILES
 do
-	ls $f >> output_test.log && cat $f | ./lem-in -q >> output_test.log
+	ls $f | tee -a output_test.log >> valgrind.log && cat $f | valgrind --log-fd=9 9>> valgrind.log ./lem-in -q >> output_test.log 
 done
