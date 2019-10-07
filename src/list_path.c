adb90a664e78bb7dfd930d9ff8b23fbf0be21a9f
Massoud MOVAHHED <mmovahhe@e2r12p18.42.fr>
2019-10-07 18:08:40 +0200

    map first->end gere

--- src/list_path.c
+++ src/list_path.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   list_path.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jdescler <jdescler@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mmovahhe <mmovahhe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/15 19:28:40 by jdescler          #+#    #+#             */
-/*   Updated: 2019/09/15 19:28:40 by jdescler         ###   ########.fr       */
+/*   Updated: 2019/10/06 21:31:14 by mmovahhe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,20 @@ t_path		*init_path(int vertex)
 	return (list);
 }
 
+#include <stdio.h>
+
 int			push_vertex(t_path **head, int vertex)
 {
 	t_path *tmp;
-
+	
+	if (!(*head))
+	{
+		if (!((*head) = init_path(vertex)))
+			return (-1);
+		return (1);
+	}
 	tmp = *head;
-	while (tmp->next)
+	while (tmp && tmp->next)
 		tmp = tmp->next;
 	if (!(tmp->next = init_path(vertex)))
 		return (-1);
